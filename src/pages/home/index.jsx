d64edecf74d0b43dd41bfceeb326481d2fd305ab
abhishekkhot <abhishek.khot@idfy.com>
2023-04-29 11:09:52 +0530

    query items bar added

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,30 +3,81 @@ import './style.css'
 export default function Home() {
     return (
         <div className='home-container'>
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
+            <div className='query-container'>
+                <span className='query-item'>All</span>
+                <span className='query-item'>Trending</span>
+                <span className='query-item'>Music</span>
+                <span className='query-item'>Kids</span>
+                <span className='query-item'>Editing</span>
+                <span className='query-item'>HTML</span>
+                <span className='query-item'>React tutorials</span>
+                <span className='query-item'>Recently uploaded</span>
+                <span className='query-item'>Cars</span>
+                <span className='query-item'>Circket</span>
+                <span className='query-item'>New to you</span>
+                <span className='query-item'>Drama</span>
+                <span className='query-item'>Action</span>
+                <span className='query-item'>News</span>
+                <span className='query-item'>Live</span>
 
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
+                <span className='query-item'>All</span>
+                <span className='query-item'>Trending</span>
+                <span className='query-item'>Music</span>
+                <span className='query-item'>Kids</span>
+                <span className='query-item'>Editing</span>
+                <span className='query-item'>HTML</span>
+                <span className='query-item'>React tutorials</span>
+                <span className='query-item'>Recently uploaded</span>
+                <span className='query-item'>Cars</span>
+                <span className='query-item'>Circket</span>
+                <span className='query-item'>New to you</span>
+                <span className='query-item'>Drama</span>
+                <span className='query-item'>Action</span>
+                <span className='query-item'>News</span>
+                <span className='query-item'>Live</span>
+            </div>
+            <div className="home-video-container">
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
 
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
 
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
 
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
-            <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+                <div className='video-conainter'>Video</div>
+            </div>
+
         </div>
     )
 }
